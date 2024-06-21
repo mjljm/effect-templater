1db4b930edf9b5c6efb781a2154e77e64f43bfee
martinjeromelouis <32217468+martinjeromelouis@users.noreply.github.com>
2024-06-21 16:16:08 +0200

    Use deepmerge to merge package.json's

--- project.config.js
+++ project.config.js
@@ -1,28 +1,28 @@
 import * as ConfigManager from '@mjljm/configs-manager';
+import merge from 'deepmerge';
 
 const packageName = 'effect-templater';
-const packageJson = {
-	...ConfigManager.PackageBase,
-	...ConfigManager.PackageSubRepo,
-	...ConfigManager.PackageEffectTags,
-	...ConfigManager.PackageSubRepoPublic,
-	name: ConfigManager.PackageBase.name(packageName),
-	version: '1.0.0',
-	repository: ConfigManager.PackageBase.repository(packageName),
-	bugs: ConfigManager.PackageBase.bugs(packageName),
-	homepage: ConfigManager.PackageBase.homepage(packageName),
-	description: 'A complement to the official effect library dedicated to templating',
-	scripts: {
-		...ConfigManager.PackageSubRepo.scripts,
-		...ConfigManager.PackageBase.scripts,
-		...ConfigManager.PackageSubRepoPublic.scripts
-	},
-	peerDependencies: {
-		'@mjljm-dev/effect-lib': 'workspace:*',
-		'@mjljm-dev/js-lib': 'workspace:*',
-		effect: ConfigManager.Versions.effectVersion
+
+const packageJson = merge.all([
+	ConfigManager.PackageBase,
+	ConfigManager.PackageSubRepo,
+	ConfigManager.PackageEffectTags,
+	ConfigManager.PackageSubRepoPublic,
+	{
+		name: ConfigManager.PackageBase.name(packageName),
+		version: '1.0.0',
+		repository: ConfigManager.PackageBase.repository(packageName),
+		bugs: ConfigManager.PackageBase.bugs(packageName),
+		homepage: ConfigManager.PackageBase.homepage(packageName),
+		description: 'A complement to the official effect library dedicated to templating',
+		peerDependencies: {
+			'@mjljm-dev/effect-lib': 'workspace:*',
+			'@mjljm-dev/js-lib': 'workspace:*',
+			effect: ConfigManager.Versions.effectVersion
+		}
 	}
-};
+]);
+
 export default {
 	// Put prettier in first position so the next generated files will get formatted
 	'prettier.config.js': ConfigManager.PrettierConfigTemplate,
